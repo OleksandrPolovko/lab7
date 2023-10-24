9958c993ce62cc5e4088ac4c3df7df30ee843897
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-10-24 19:09:41 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+        {
+            // Для цілочисельних значень
+            Calculator<int> intCalculator = new Calculator<int>((a, b) => a + b, (a, b) => a - b, (a, b) => a * b, (a, b) => a / b);
+
+            int result = intCalculator.PerformOperation(5, 3, intCalculator.Add);
+            Console.WriteLine($"5 + 3 = {result}");
+
+            result = intCalculator.PerformOperation(5, 3, intCalculator.Subtract);
+            Console.WriteLine($"5 - 3 = {result}");
+
+            result = intCalculator.PerformOperation(5, 3, intCalculator.Multiply);
+            Console.WriteLine($"5 * 3 = {result}");
+
+            result = intCalculator.PerformOperation(6, 3, intCalculator.Divide);
+            Console.WriteLine($"6 / 3 = {result}");
+
+            // Для дробових значень
+            Calculator<double> doubleCalculator = new Calculator<double>((a, b) => a + b, (a, b) => a - b, (a, b) => a * b, (a, b) => a / b);
+
+            double doubleResult = doubleCalculator.PerformOperation(5.5, 2.2, doubleCalculator.Add);
+            Console.WriteLine($"5.5 + 2.2 = {doubleResult}");
+
+            doubleResult = doubleCalculator.PerformOperation(5.5, 2.2, doubleCalculator.Subtract);
+            Console.WriteLine($"5.5 - 2.2 = {doubleResult}");
+
+            doubleResult = doubleCalculator.PerformOperation(5.5, 2.2, doubleCalculator.Multiply);
+            Console.WriteLine($"5.5 * 2.2 = {doubleResult}");
+
+            doubleResult = doubleCalculator.PerformOperation(6.6, 3.3, doubleCalculator.Divide);
+            Console.WriteLine($"6.6 / 3.3 = {doubleResult}");
+        }
+    }
+}
+
