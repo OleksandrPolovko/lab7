3e6cae2e20e77c0642adb03929ea15be4553f817
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-10-25 12:08:53 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Lab7
 {
     internal class Program
     {
-            static void Main(string[] args)
+            static void Task1()
         {
             // Для цілочисельних значень
             Calculator<int> intCalculator = new Calculator<int>((a, b) => a + b, (a, b) => a - b, (a, b) => a * b, (a, b) => a / b);
@@ -40,6 +40,44 @@ namespace Lab7
             doubleResult = doubleCalculator.PerformOperation(6.6, 3.3, doubleCalculator.Divide);
             Console.WriteLine($"6.6 / 3.3 = {doubleResult}");
         }
+        
+            static void Task2()
+        {
+            // Створюємо репозиторій для рядків
+            Repository<string> stringRepository = new Repository<string>();
+
+            // Додаємо рядки до репозиторію
+            stringRepository.Add("apple");
+            stringRepository.Add("banana");
+            stringRepository.Add("cherry");
+            stringRepository.Add("date");
+
+            // Створюємо критерій для пошуку
+            Criteria<string> criteria = s => s.Length == 5; // Знайти рядки з довжиною 5 символів
+
+            // Виконуємо пошук
+            List<string> result = stringRepository.Find(criteria);
+
+            // Виводимо результат
+            Console.WriteLine("Результат пошуку:");
+            foreach (string item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+            private static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+
+            // delay to read from screen
+            Console.WriteLine("Press any key to finnish this program...");
+            Console.Read();
+        }
     }
 }
+
 
